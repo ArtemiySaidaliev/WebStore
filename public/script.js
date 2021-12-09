0b8e5ae50a275073cd3b19d196bfdbd696398c63
siera <35771069+ArtemiySaidaliev@users.noreply.github.com>
2021-12-09 13:16:00 +0300

    add available check and catalog view

--- public/script.js
+++ public/script.js
@@ -1,21 +1,55 @@
 let skiBtn = document.getElementById("skiBtn")
-let startBtn = document.getElementById("startBtn")
 let boardBtn = document.getElementById("boardBtn")
 let gearBtn = document.getElementById("gearBtn")
+let startBtn = document.getElementById("startBtn")
 skiBtn.addEventListener('click', function(){
     alert('Ski')
-})
-startBtn.addEventListener('click', function(){
     let xhr = new XMLHttpRequest()
-    xhr.open('GET','/articles',true)
-    xhr.onreadystatechange = function(){
-        if (xhr.readyState != 4) return content.innerHTML.responseText
+    let message = {
+        "type" : "ski"
+    }
+    xhr.open('GET','/available?message=' + JSON.stringify(message),true)
+    xhr.onreadystatechange=function(){
+        if(xhr.readyState != 4)return
+        alert(xhr.responseText)
     }
     xhr.send()
 })
+
 boardBtn.addEventListener('click', function(){
-    alert('board')
+    alert('Snow board')
+    let xhr = new XMLHttpRequest()
+    let message = {
+        "type" : "snowboard"
+    }
+    xhr.open('GET','/available?message=' + JSON.stringify(message),true)
+    xhr.onreadystatechange=function(){
+        if(xhr.readyState != 4)return
+        alert(xhr.responseText)
+    }
+    xhr.send()
 })
+
 gearBtn.addEventListener('click', function(){
-    alert('gear')
+    alert('Snow board')
+    let xhr = new XMLHttpRequest()
+    let message = {
+        "type" : "helmet"
+    }
+    xhr.open('GET','/available?message=' + JSON.stringify(message),true)
+    xhr.onreadystatechange=function(){
+        if(xhr.readyState != 4)return
+        alert(xhr.responseText)
+    }
+    xhr.send()
+})
+
+startBtn.addEventListener('click', function(){
+    let xhr = new XMLHttpRequest()
+    xhr.open('GET','/articles',true)
+    xhr.onreadystatechange=function(){
+        if(xhr.readyState != 4)return
+        alert(xhr.responseText)
+    }
+    xhr.send()
 })
--- server.js
+++ server.js
@@ -2,13 +2,25 @@ let http=require('http')
 let static=require('node-static')
 let url=require('url')
 let file=new static.Server('./public')
+
 function accept(req,res){
     let urlParsed=url.parse(req.url,true).pathname
+    let urlObj=url.parse(req.url,true)
     if(urlParsed=='/articles'){
-        res.end('Товар!')
+        res.end('We have: long ski,medium ski, short ski, snowboard red, snowboard blue, snowboard yellow and helmet')
     }
     if(urlParsed=='/available'){
-        res.end(' Доступный Товар!')
+        let message = urlObj.query.message
+        if(message.type == 'ski'){
+            res.end('Available: long ski, short ski')
+        }
+        if(message.type == 'snowboard'){
+            res.end('Available: snowboard blue')
+        }
+        if(message.type == 'helmet'){
+            res.end('helmets out of stock')
+        }
+
     }
     else{file.serve(req,res)}
 }
